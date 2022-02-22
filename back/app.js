fca2843a1919c869e1b42abf231070ac1b538d57
Jacky Wong <jacky.wong@sfr.fr>
2022-02-22 01:22:47 +0100

    ajout de dotenv

--- back/app.js
+++ back/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 
 const userRoutes = require('./routes/user')
 const sauceRoutes = require('./routes/sauce')
 const path = require('path')
 
+
 // connexion à la base de donnée
 
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://SuperAdmin:VeryComplicatedBecauseItsCool94400@mycluster.csty8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(process.env.SECRET_MDB,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -22,12 +24,20 @@ mongoose.connect('mongodb+srv://SuperAdmin:VeryComplicatedBecauseItsCool94400@my
 
 const app = express();
 
+
 // configuration CORS
 
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
     next();
 });
 
@@ -40,6 +50,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
 
 
 module.exports = app;
